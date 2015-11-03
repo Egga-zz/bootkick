511424cad3d76dfe7922b3c2e82bc4f10423eca1
Egga (Jan Hartung) <github@egga.de>
2015-11-03 16:05:46 +0100

    optimize imports

--- src/test/java/de/egga/persons/PersonControllerTest.java
+++ src/test/java/de/egga/persons/PersonControllerTest.java
@@ -5,13 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
